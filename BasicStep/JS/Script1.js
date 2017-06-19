403e1f24dafc0d02e96f1c593398137a59195414
KoreanJungYJ <jung22337@naver.com>
2017-06-19 15:47:37 +0900

    ClickEvent

--- BasicStep/JS/Script1.js
+++ BasicStep/JS/Script1.js
@@ -1,6 +1,12 @@
-function clickMe(){
+$(function(){
+    $('#clickBox2').on('click',function(){
+        $('#clickBox2').css('color','white');
+    });
+    function clickMe(){
     $('#clickBox').css({
         'background-color':'black',
         'transform':'scale(2)'
     })
-}
+    };
+});
+
