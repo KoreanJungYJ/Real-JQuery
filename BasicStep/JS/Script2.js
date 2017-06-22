cc007ff694498109fc4e5e274cfdf79b20c8b675
KoreanJungYJ <jung22337@naver.com>
2017-06-22 23:40:42 +0900

    delete background

--- BasicStep/JS/Script2.js
+++ BasicStep/JS/Script2.js
@@ -3,7 +3,7 @@ $(function(){
 
     $('#box1').on('mouseover',function(){
         $(this).stop(true).animate({
-            backgroundColor: '#65ce47',
+//            backgroundColor: '#65ce47',
             borderRadius:'10px',
             width: '400px',
             height: '500px'
@@ -11,7 +11,7 @@ $(function(){
     })
     .on('mouseout',function(){
         $(this).stop(true).animate({
-            backgroundColor:'#59cf8c',
+//            backgroundColor:'#59cf8c',
             borderRadius:'0px',
             width:'200px',
             height:'100px'
